69ebae4b38c73ec07a9205bf40715657160f9bc9
mostafamt <mostafamt60@yahoo.com>
2019-06-20 13:26:31 +0200

    remove verifyEmail

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
