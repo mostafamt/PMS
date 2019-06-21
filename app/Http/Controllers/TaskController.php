59469b4b9a4fd3561521ca163dd5b6550d08356b
abdelhamidelsayed <abdelhamidelsayed187@gmail.com>
2019-06-21 14:02:13 +0200

    add super visor

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Project;
 use App\Task;
+use App\User;
+
 
 class TaskController extends Controller
 {
@@ -18,6 +20,8 @@ class TaskController extends Controller
 
     public function store($id , Request $request)
     {
+
+
         $task = new Task(array(
             'name' => $request->get('name') ,
             'start_date' => $request->get('start_date'),
@@ -29,8 +33,45 @@ class TaskController extends Controller
         return redirect(action('ProjectController@edit' , $id ))->with('status' , 'Task created successfully!');
     }
 
+
     public function edit($project_id , $task_id)
     {
+
         return view('tasks.edit');
     }
+    public function addsupervisor($id )
+    {
+           $task = Task::whereId($id)->firstOrFail();
+           $project = Project::where('id',$task->project_id)->first();
+           $user=User::all();
+
+           return view('tasks.addsupervisor' , compact('project','task'));
+    }
+       public function savesupervisor(Request $request,$id )
+    {
+      $task=Task::find($id);
+      $username=$request->input('supervisor');
+      $user=User::where('user_name',$username)->first();
+      if($user){
+
+         $task->name=$request->input('name');
+         $task->start_date=$request->input('start_date');
+         $task->end_date=$request->input('end_date');
+         $task->project_id=$request->input('project');
+         $task->description=$request->input('desc');
+         $task->user_id=$user->id;
+         $task->save();
+      return redirect(route('project.show' , $request->input('project') ))->with('status' , 'super Visor added successfully');
+
+
+     }else{
+        return redirect(route('project.show' , $request->input('project') ))->with('status' , 'super Visor not Exist');
+     }
+        
+
+
+
+
+    }
+  
 }
---
+++ resources/views/tasks/addsupervisor.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form method="post" action="{{ route('savesupervisor',$task->id) }}">
+         <input type="hidden" name="_method" value="PUT">
+
+        @csrf
+        @foreach ($errors->all() as $error)
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ $error }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endforeach
+        @if (session('status'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('status') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endif
+        <legend>Add Super Visor</legend>
+        <div class="form-group">
+           
+            <input type="hidden" class="form-control" id="formGroupExampleInput" placeholder="Project Name" name="name"
+                value="{!! $task->name !!}">
+        
+          <div class="form-group">
+            <label for="formGroupExampleInput">User Name</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="user name of super visor" name="supervisor">
+               
+        </div>
+
+            <input  name="project" value="{{ $project->id }}"  type="hidden" />
+
+            
+        <div class="form-group">
+            <!-- Date input -->
+          
+            <input class="form-control" id="start_date" name="start_date" value="{!! $project->start_date !!}"
+                type="hidden" />
+        </div>
+
+        <div class="form-group">
+            <!-- Date input -->
+           
+            <input class="form-control" id="end_date" name="end_date" value="{!! $project->end_date !!}"
+                type="hidden" />
+        </div>
+
+
+{{--
+            <!-- Date input -->
+           
+            <input class="form-control" id="start_date" name="start_date" value="{!! $task->start_date !!}" 
+                type="hidden" />
+      
+
+     
+            <input class="form-control" id="end_date" name="end_date" value="{!! $task->end_date !!}"
+                type="hidden" />
+      
+
+       --}}
+         
+             <input class="form-control" id="end_date" name="desc" value="{!! $task->description !!}"  type="hidden" />
+           
+       
+
+        
+
+        <button type="submit" class="btn btn-primary">Update</button>
+    </form>
+</div>
+
+<script>
+    $(document).ready(function(){
+                  var start_date=$('input[id="start_date"]'); //our date input has the name "date"
+                  var end_date=$('input[id="end_date"]');
+                  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+                  var options={
+                    format: 'yyyy/mm/dd',
+                    container: container,
+                    todayHighlight: true,
+                    autoclose: true,
+                  };
+                  start_date.datepicker(options);
+                  end_date.datepicker(options);
+                });
+</script>
+
+@endsection
